f37721a0df1983eb0b78faf2109a599c60310e48
gnostertag <ostertag2000@gmail.com>
2023-05-26 13:42:15 -0700

    styling changes

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -5,10 +5,10 @@ function Experience() {
     <> 
     <div className='experience'>
       <h1>About Me</h1>
-      <img src={Image} width='200px' alt='profile'/>
-    <p> I am a dynamic and versatile professional with a unique background in various industries. From my experience as a tree climber, I have developed a strong work ethic and a keen eye for detail. As a loss prevention specialist, I honed my analytical and problem-solving skills, while my service in the United States Navy instilled in me discipline, leadership, and teamwork.</p>
-    <p> Now, as a software engineering student, I am excited to bring my diverse skill set and experience to the tech industry. I am passionate about coding, programming, and software development and have gained proficiency in various programming languages and frameworks such as Javascript, Ruby, SQL, HTML, CSS, React, and Ruby on Rails.</p>
-    <p> As a highly motivated and adaptive individual, I am committed to continuing my education, and using my skills to create innovative solutions. I look forward to connecting with other professionals in the software development field and exploring opportunities to collaborate and make a meaningful impact.</p>
+      <img src={Image} width='200px' alt='profile' className='profile-pic'/>
+    <p className='paragraph-1'> I am a dynamic and versatile professional with a unique background in various industries. From my experience as a tree climber, I have developed a strong work ethic and a keen eye for detail. As a loss prevention specialist, I honed my analytical and problem-solving skills, while my service in the United States Navy instilled in me discipline, leadership, and teamwork.</p>
+    <p className='paragraph-2'> Now, as a software engineering student, I am excited to bring my diverse skill set and experience to the tech industry. I am passionate about coding, programming, and software development and have gained proficiency in various programming languages and frameworks such as Javascript, Ruby, SQL, HTML, CSS, React, and Ruby on Rails.</p>
+    <p className='paragraph-3'> As a highly motivated and adaptive individual, I am committed to continuing my education, and using my skills to create innovative solutions. I look forward to connecting with other professionals in the software development field and exploring opportunities to collaborate and make a meaningful impact.</p>
     </div>
     </>
   )
