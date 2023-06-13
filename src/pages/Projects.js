e681a1190b77a0d0b01a83ed56a6f345c02d4f16
gnostertag <ostertag2000@gmail.com>
2023-06-13 12:40:38 -0700

    project card added

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardBody, CardTitle, CardText, Button } from "reactstrap";
 import Image from "../assets/TicTacToe image.png";
 import Gtai from "../assets/GTAI.png";
+import Treasure from "../assets/treasure-hunt-screenshot.png"
 function Projects() {
   return (
     <>
@@ -40,6 +41,27 @@ function Projects() {
             </CardText>
             <Button
               href={"https://gtai-frontend.onrender.com/"}
+              className="button-3"
+            >
+              See Project
+            </Button>
+          </CardBody>
+        </Card>
+        <Card
+          style={{
+            width: "18rem",
+          }}
+        >
+          <img alt="Sample" src={Treasure} className="tic-image" />
+          <CardBody>
+            <CardTitle tag="h5">Treasure Hunt</CardTitle>
+
+            <CardText>
+              Full-Stack car buying, selling, and stealing application created
+              using React and Rails
+            </CardText>
+            <Button
+              href={""}
               className="button-3"
             >
               See Project
